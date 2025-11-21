e25b036e8592fff8c3c65a6679e2a012bf1b4824
calliope-net <141074597+calliope-net@users.noreply.github.com>
2025-11-21 20:43:08 +0100

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -24,13 +24,13 @@ function Ampel3 (ro: number, ge: number, gr: number) {
     pins.gpio_writeBit(pins.pins_gpio_I2C_ADDRESS(pins.gpio_eI2C_ADDRESS.GPIO_x27), pins.pins_gpio_pin(pins.gpio_epin.Pin1), ge == 1)
     pins.gpio_writeBit(pins.pins_gpio_I2C_ADDRESS(pins.gpio_eI2C_ADDRESS.GPIO_x27), pins.pins_gpio_pin(pins.gpio_epin.Pin2), gr == 1)
 }
+function i2cgpioampel541 () {
+    pins.comment(pins.pins_text("calliope-net/i2c-gpio-ampel5-41"))
+}
 function Ampel2 (ro: number, gr: number) {
     pins.comment(pins.pins_text("Fußgänger Pin4=rot Pin5=grün"))
     pins.gpio_writeBit(pins.pins_gpio_I2C_ADDRESS(pins.gpio_eI2C_ADDRESS.GPIO_x27), pins.pins_gpio_pin(pins.gpio_epin.Pin4), ro == 1)
     pins.gpio_writeBit(pins.pins_gpio_I2C_ADDRESS(pins.gpio_eI2C_ADDRESS.GPIO_x27), pins.pins_gpio_pin(pins.gpio_epin.Pin5), gr == 1)
-}
-function i2cgpioampel541 () {
-    pins.comment(pins.pins_text("calliope-net/i2c-gpio-ampel5-41"))
 }
 pins.gpio_setMode(
 pins.pins_gpio_I2C_ADDRESS(pins.gpio_eI2C_ADDRESS.GPIO_x27),
@@ -43,6 +43,13 @@ pins.gpio_eIO.OUT,
 pins.gpio_eIO.OUT,
 pins.gpio_eIO.OUT
 )
+basic.showLeds(`
+    # . # . .
+    . . . . .
+    # . # . .
+    . . . . .
+    # . . # .
+    `)
 loops.everyInterval(1000, function () {
     pins.comment(pins.pins_text("Pin6=Taste an der Ampel"))
     if (pins.gpio_readBit(pins.pins_gpio_I2C_ADDRESS(pins.gpio_eI2C_ADDRESS.GPIO_x27), pins.pins_gpio_pin(pins.gpio_epin.Pin6))) {
